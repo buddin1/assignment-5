0e955e49e5064153809a82877ffd39641996fa3a
buddin1 <buddin70755@gmail.com>
2025-08-27 23:40:07 +0600

    '

---
+++ index.js
@@ -0,0 +1,20 @@
+// heart-button click js
+function heartClick(id) {
+ document.getElementById(id).addEventListener('click', function() {
+    heartElement = document.getElementById(id);
+    heartCountElement = document.getElementById('heart-count'); 
+    let count = parseInt(heartCountElement.innerText);
+    count += 1;
+    heartCountElement.innerText = count;
+    
+});   
+}
+heartClick('heart1');
+heartClick('heart2');
+heartClick('heart3');
+heartClick('heart4');   
+heartClick('heart5');
+heartClick('heart6');
+heartClick('heart7');
+heartClick('heart8');
+heartClick('heart9');
